69d25298f7427ecac2e59f6555bcdc54fa0d67cd
Thomas Aven <avensin@hotmail.com>
2025-05-25 18:20:26 +0200

    only recompiled new and changed shaders

--- assets/shaders/compile.sh
+++ assets/shaders/compile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-glslangValidator -V basic.vert -o basic.vert.spv
-glslangValidator -V basic.frag -o basic.frag.spv
-glslangValidator -V skybox.vert -o skybox.vert.spv
-glslangValidator -V skybox.frag -o skybox.frag.spv
-glslangValidator -V grid.vert -o grid.vert.spv
-glslangValidator -V grid.frag -o grid.frag.spv
-glslangValidator -V plane.vert -o plane.vert.spv
-glslangValidator -V plane.frag -o plane.frag.spv
+set -e
+
+cd "$(dirname "$0")"
+IFS=$'\n' shaders=$(git ls-files --modified --others --exclude-standard)
+for shader in ${shaders[@]}; do
+	case $shader in
+		*.vert | *.vert | *.comp)
+			glslangValidator -V "${shader}" -o "${shader}.spv"
+			;;
+	esac
+done
